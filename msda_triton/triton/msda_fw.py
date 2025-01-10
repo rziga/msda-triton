ade23103b17d7c99c1109a72a655b3ad8b720231
Ziga R. <ziga.mlearning@gmail.com>
2025-01-10 13:11:44 +0100

    upgraded bilinear sampling to deformable attention

--- msda_triton/triton/msda_fw.py
+++ msda_triton/triton/msda_fw.py
@@ -2,109 +2,169 @@ import triton
 from triton import language as tl
 
 @triton.jit
-def get_nearest(coor):
+def _get_nearest(coor):
     prev = tl.cast(tl.floor(coor), tl.int64)
     next = prev + 1
     return prev, next
 
 @triton.jit
-def sample_point(img_ptr, y, x, W, C, BLOCK_SIZE_C):
+def _get_point(img_ptr, y, x, W, C, BLOCK_SIZE_C):
+    c = tl.arange(0, BLOCK_SIZE_C)
     offsets = (
-          W*C * y[:, None]
-        +   C * x[:, None]
-        +   1 * tl.arange(0, BLOCK_SIZE_C)[None, :] 
+          W*C * y[:, :, None]
+        +   C * x[:, :, None]
+        +   1 * c[None, None, :]
     )
-    mask = tl.arange(0, BLOCK_SIZE_C)[None, :] < C
+    mask = c[None, None, :] < C
     return tl.load(
         img_ptr+offsets,
         mask
     )
 
 @triton.jit
-def bilinear_sample_kernel(
-    out_ptr, # output pointer
-    img_ptr, # image pointer
-    smp_ptr, # sampling points pointer
-    H, # image height
-    W, # image width
-    C, # image channels
-    S, # number of sampling points
-    BLOCK_SIZE_S: tl.constexpr,
-    BLOCK_SIZE_C: tl.constexpr, # assume BLOCK_SIZE_C > C
-):
-    #img_start = tl.program_id
-    smp_start = tl.program_id(0) * BLOCK_SIZE_S
-    smp_size = S * 2
-
-    # load the sampling points
-    smp_offsets = (
-        + 2 * (smp_start + tl.arange(0, BLOCK_SIZE_S))[:, None]
-        + 1 *              tl.arange(0,            2) [None, :]
-    )
-    smp_mask = smp_offsets < smp_size
-    sampling_points = tl.load(
-        smp_ptr+smp_offsets,
-        smp_mask
-    ) # [Q, 2]
+def _sample_bilinear(img_ptr, sampling_points, H, W, C, C_BLOCK):
     
     # unnormalize coordinates
     x, y = tl.split(sampling_points)
-    x = x * (W - 1) # [Q]
-    y = y * (H - 1) # [Q]
+    x = x * (W - 1) # [N, P]
+    y = y * (H - 1) # [N, P]
 
     # calculate the xy coordinates of nearest pixels
-    xl, xr = get_nearest(x) # [Q], [Q]
-    yt, yb = get_nearest(y) # [Q], [Q]
+    xl, xr = _get_nearest(x) # [N, P], [N, P]
+    yt, yb = _get_nearest(y) # [N, P], [N, P]
     
     # sample the neighbors
-    img_tl = sample_point(img_ptr, yt, xl, W, C, BLOCK_SIZE_C) # [Q, C]
-    img_tr = sample_point(img_ptr, yt, xr, W, C, BLOCK_SIZE_C) # [Q, C]
-    img_bl = sample_point(img_ptr, yb, xl, W, C, BLOCK_SIZE_C) # [Q, C]
-    img_br = sample_point(img_ptr, yb, xr, W, C, BLOCK_SIZE_C) # [Q, C]
+    img_tl = _get_point(img_ptr, yt, xl, W, C, C_BLOCK) # [N, P, C]
+    img_tr = _get_point(img_ptr, yt, xr, W, C, C_BLOCK) # [N, P, C]
+    img_bl = _get_point(img_ptr, yb, xl, W, C, C_BLOCK) # [N, P, C]
+    img_br = _get_point(img_ptr, yb, xr, W, C, C_BLOCK) # [N, P, C]
 
     # calculate sampling weights
-    wei_tl, wei_tr = (yb-y)*(xr-x), (yb-y)*(x-xl) # [Q], [Q]
-    wei_bl, wei_br = (y-yt)*(xr-x), (y-yt)*(x-xl) # [Q], [Q]
+    wei_tl, wei_tr = (yb-y)*(xr-x), (yb-y)*(x-xl) # [N, P], [N, P]
+    wei_bl, wei_br = (y-yt)*(xr-x), (y-yt)*(x-xl) # [N, P], [N, P]
 
     # sum the contribution of neighboring points
-    sample = (
-          wei_tl[:, None]*img_tl + wei_tr[:, None]*img_tr
-        + wei_bl[:, None]*img_bl + wei_br[:, None]*img_br
-    ) # [Q, C]
+    samples = (
+          wei_tl[:, :, None]*img_tl + wei_tr[:, :, None]*img_tr
+        + wei_bl[:, :, None]*img_bl + wei_br[:, :, None]*img_br
+    ) # [N, P, C]
+
+    return samples
+
+@triton.jit
+def deformable_att_kernel(
+    out_ptr, # output pointer           - [B, N, C]
+    img_ptr, # image pointer            - [B, H, W, C]
+    smp_ptr, # sampling points pointer  - [B, N, P, C]
+    wei_ptr, # att weights pointer      - [B, N, P]
+    H, # image height
+    W, # image width
+    C, # image channels
+    N, # number of point groups
+    P, # number of points
+    N_BLOCK: tl.constexpr, # assume BLOCK_SIZE_N > N
+    C_BLOCK: tl.constexpr, # assume BLOCK_SIZE_C > C
+    P_BLOCK: tl.constexpr, # assume BLOCK_SIZE_P > P
+):
+    # get program ids
+    pid_batch = tl.program_id(0) # id of batch
+    
+    # compute offsets
+    smp_size = N * P * 2
+    smp_ptr = smp_ptr + pid_batch * smp_size
+
+    wei_size = N * P
+    wei_ptr = wei_ptr + pid_batch * wei_size
+    
+    img_size = H * W * C
+    img_ptr = img_ptr + pid_batch * img_size
+    
+    out_size = N * C
+    out_ptr = out_ptr + pid_batch * out_size
+
+    # compute ranges and masks
+    N_block_offsets = tl.arange(0, N_BLOCK)
+    N_block_mask = N_block_offsets < N
+
+    C_block_offsets = tl.arange(0, C_BLOCK)
+    C_block_mask = C_block_offsets < C
+
+    P_block_offsets = tl.arange(0, P_BLOCK)
+    P_block_mask = P_block_offsets < P
+
+    # load the sampling points
+    smp_offsets = (
+        2*P * N_block_offsets[:, None, None]
+        + 2 * P_block_offsets[None, :, None]
+        + 1 * tl.arange(0, 2)[None, None, :]
+    ) # [N, P, 2]
+    smp_mask = (
+          N_block_mask[:, None, None]
+        & P_block_mask[None, :, None]        
+    ) # [N, P, 1]
+    sampling_points = tl.load(
+        smp_ptr+smp_offsets,
+        smp_mask
+    ) # [N, P, 2]
+
+    # load the att weights
+    wei_offsets = (
+          P * N_block_offsets[:, None]
+        + 1 * P_block_offsets[None, :]
+    ) # [N, P]
+    wei_mask = (
+          N_block_mask[:, None]
+        & P_block_mask[None, :]
+    ) # [N, P]
+    att_weights = tl.load(
+        wei_ptr+wei_offsets,
+        wei_mask
+    ) # [N, P]
+
+    samples = _sample_bilinear(img_ptr, sampling_points, H, W, C, C_BLOCK)
+
+    # weigh the points in samples
+    weighted = tl.sum(
+        att_weights[:, :, None] * samples,
+        axis=1
+    ) # [N, P, 1] * [N, P, C] -> [N, C]
 
     # store in the output
     out_offsets = (
-          C * (smp_start + tl.arange(0, BLOCK_SIZE_S))[:, None]
-        + 1 * (            tl.arange(0, BLOCK_SIZE_C))[None, :]
-    ) # [Q, C]
+          C * N_block_offsets[:, None]
+        + 1 * C_block_offsets[None, :]
+    ) # [N, C]
     out_mask = (
-          (smp_start + tl.arange(0, BLOCK_SIZE_S) < S)[:, None]
-        & (            tl.arange(0, BLOCK_SIZE_C) < C)[None, :]
-    ) # [Q, C]
+          N_block_mask[:, None]
+        & C_block_mask[None, :]
+    )
     tl.store(
         out_ptr+out_offsets,
-        sample,
+        weighted,
         out_mask,
     )
 
-def bilinear_sample_triton(img, sampling_points):
+def triton_deformable_att(img, sampling_points, att_weights):
     B, H, W, C = img.shape
-    B, Ns, _ = sampling_points.shape
+    B, N, P, _ = sampling_points.shape
 
     # prepare output buffer
-    output = sampling_points.new_empty(B, Ns, C)
+    output = sampling_points.new_empty(B, N, C)
 
     # call the kernel
-    BLOCK_SIZE_Ns = triton.next_power_of_2(Ns)
-    BLOCK_SIZE_Ns = 32
-    NUM_PROGS_Ns  = triton.cdiv(Ns, BLOCK_SIZE_Ns)
+    BLOCK_SIZE_N = triton.next_power_of_2(N)
     BLOCK_SIZE_C = triton.next_power_of_2(C)
-    bilinear_sample_kernel[(NUM_PROGS_Ns, 1, 1)](
+    BLOCK_SIZE_P = triton.next_power_of_2(P)
+    NUM_PROGS_B = B
+    deformable_att_kernel[(NUM_PROGS_B, 1, 1)](
         output,
         img,
         sampling_points,
-        H, W, C, Ns, 
-        BLOCK_SIZE_Ns, BLOCK_SIZE_C
+        att_weights,
+        H, W, C, N, P,
+        BLOCK_SIZE_N,
+        BLOCK_SIZE_C,
+        BLOCK_SIZE_P,
     )
 
     return output
--- tests/test_kernel.py
+++ tests/test_kernel.py
@@ -1,30 +1,33 @@
 import torch
 
-from msda_triton.triton.msda_fw import bilinear_sample_triton
+from msda_triton.triton.msda_fw import triton_deformable_att
 
 def test_kernel():
-    img = torch.rand(1, 128, 128, 256).cuda()
-    sampling_points = torch.rand(1, 128, 2).cuda()
+    img = torch.rand(6, 128, 128, 256).cuda()
+    sampling_points = torch.rand(6, 128, 4, 2).cuda()
+    att_weights = torch.rand(6, 128, 4).cuda()
     
-    true = bilinear_sample_torch(img, sampling_points)
-    test = bilinear_sample_triton(img, sampling_points)
+    true = torch_deformable_att(img, sampling_points, att_weights)
+    test = triton_deformable_att(img, sampling_points, att_weights)
 
     torch.testing.assert_close(test, true)
 
-def bilinear_sample_torch(img, sampling_points):
+def torch_deformable_att(img, sampling_points, att_weights):
     """
     img - [B, H, W, C]
-    sampling_points - [B, N, 2] in xy relative [0-1]
+    sampling_points - [B, N, P, 2] in xy relative [0-1]
+    att_weights - [B, N, P]
 
-    outputs: samples - [B, N]
+    outputs: samples - [B, N, C]
     """
-    B, H, W, _ = img.shape
+    B, H, W, C = img.shape
+    B, N, P, _ = sampling_points.shape
     device = img.device
     
     # unnormalize coordinates
     x, y = sampling_points.unbind(-1)
-    x = x * (W-1)
-    y = y * (H-1)
+    x = x * (W-1) # [B, N, P]
+    y = y * (H-1) # [B, N, P]
 
     # get idxs for interpolation
     xl = x.floor().long()
@@ -35,16 +38,16 @@ def bilinear_sample_torch(img, sampling_points):
     yb_ = yb.clamp(max=H-1)
     
     # get the interpolation point values
-    b = torch.arange(B, device=device)[:, None]
-    point_tl, point_tr = img[b, yt , xl, :], img[b, yt , xr_, :]
-    point_bl, point_br = img[b, yb_, xl, :], img[b, yb_, xr_, :]
+    b = torch.arange(B, device=device)[:, None, None]
+    point_tl, point_tr = img[b, yt , xl, :], img[b, yt , xr_, :] # [B, N, P, C], [B, N, P, C]
+    point_bl, point_br = img[b, yb_, xl, :], img[b, yb_, xr_, :] # [B, N, P, C], [B, N, P, C]
     #print("point:")
     #print(point_tl, point_tr)
     #print(point_bl, point_br)
 
     # get the interpolation weights
-    weight_tl, weight_tr = (yb-y)*(xr-x), (yb-y)*(x-xl)
-    weight_bl, weight_br = (y-yt)*(xr-x), (y-yt)*(x-xl)
+    weight_tl, weight_tr = (yb-y)*(xr-x), (yb-y)*(x-xl) # [B, N, P], [B, N, P]
+    weight_bl, weight_br = (y-yt)*(xr-x), (y-yt)*(x-xl) # [B, N, P], [B, N, P]
     #print("weight:")
     #print(weight_tl, weight_tr)
     #print(weight_bl, weight_br)
@@ -53,6 +56,11 @@ def bilinear_sample_torch(img, sampling_points):
     samples = (
           weight_tl[..., None]*point_tl + weight_tr[..., None]*point_tr
         + weight_bl[..., None]*point_bl + weight_br[..., None]*point_br
-    )
+    ) # [B, N, P, C]
+    #print(samples)
 
-    return samples
+    # use attention weights
+    weighted = (samples * att_weights[..., None]).sum(-2) # [B, N, C]
+    #print(weighted)
+
+    return weighted
