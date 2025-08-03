2ba30c08b69eaccd70ae89722d6b7cf2c0e9e0c7
Ziga R. <102856773+rziga@users.noreply.github.com>
2025-08-03 13:56:02 +0200

    bump version

--- src/msda_triton/__init__.py
+++ src/msda_triton/__init__.py
@@ -1,4 +1,8 @@
+from importlib.metadata import version
 from .frontend import multiscale_deformable_attention, MultiscaleDeformableAttention
+
+
+__version__ = version("msda_triton")
 
 __all__ = [
     "multiscale_deformable_attention",
